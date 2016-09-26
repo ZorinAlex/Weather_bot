d4d1314a54d29aad93825e30899d19cf07dea8b4
ZorinAlex <zorinuk@gmail.com>
2016-09-26 21:28:24 +0300

    test

--- app.js
+++ app.js
@@ -221,9 +221,9 @@ function receivedMessage(event) {
   var timeOfMessage = event.timestamp;
   var message = event.message;
 
-  console.log("Received message for user %d and page %d at %d with message:", 
-    senderID, recipientID, timeOfMessage);
-  console.log(JSON.stringify(message));
+  //console.log("Received message for user %d and page %d at %d with message:",
+  //  senderID, recipientID, timeOfMessage);
+  //console.log(JSON.stringify(message));
 
   var isEcho = message.is_echo;
   var messageId = message.mid;
@@ -235,86 +235,90 @@ function receivedMessage(event) {
   var messageAttachments = message.attachments;
   var quickReply = message.quick_reply;
 
-  if (isEcho) {
-    // Just logging message echoes to console
-    console.log("Received echo for message %s and app %d with metadata %s", 
-      messageId, appId, metadata);
-    return;
-  } else if (quickReply) {
-    var quickReplyPayload = quickReply.payload;
-    console.log("Quick reply for message %s with payload %s",
-      messageId, quickReplyPayload);
-
-    sendTextMessage(senderID, "Quick reply tapped");
-    return;
-  }
+  //if (isEcho) {
+  //  // Just logging message echoes to console
+  //  console.log("Received echo for message %s and app %d with metadata %s",
+  //    messageId, appId, metadata);
+  //  return;
+  //} else if (quickReply) {
+  //  var quickReplyPayload = quickReply.payload;
+  //  console.log("Quick reply for message %s with payload %s",
+  //    messageId, quickReplyPayload);
+  //
+  //  sendTextMessage(senderID, "Quick reply tapped");
+  //  return;
+  //}
 
   if (messageText) {
-
-    // If we receive a text message, check to see if it matches any special
-    // keywords and send back the corresponding example. Otherwise, just echo
-    // the text we received.
     switch (messageText) {
-      case 'image':
-        sendImageMessage(senderID);
-        break;
-
-      case 'gif':
-        sendGifMessage(senderID);
-        break;
-
-      case 'audio':
-        sendAudioMessage(senderID);
-        break;
-
-      case 'video':
-        sendVideoMessage(senderID);
-        break;
-
-      case 'file':
-        sendFileMessage(senderID);
-        break;
-
-      case 'button':
-        sendButtonMessage(senderID);
-        break;
-
-      case 'generic':
-        sendGenericMessage(senderID);
-        break;
-
-      case 'receipt':
-        sendReceiptMessage(senderID);
-        break;
-
-      case 'quick reply':
+      case 'Hi':
         sendQuickReply(senderID);
-        break;        
-
-      case 'read receipt':
-        sendReadReceipt(senderID);
-        break;        
-
-      case 'typing on':
-        sendTypingOn(senderID);
-        break;        
-
-      case 'typing off':
-        sendTypingOff(senderID);
-        break;        
-
-      case 'account linking':
-        sendAccountLinking(senderID);
         break;
-
-      default:
-        sendTextMessage(senderID, messageText);
+      case 'Hello':
+        sendTextMessage(senderID, metadata);
+        break;
     }
-  } else if (messageAttachments) {
-    sendTextMessage(senderID, "Message with attachment received");
+    //  switch (messageText) {
+    //    case 'image':
+    //      sendImageMessage(senderID);
+    //      break;
+    //
+    //    case 'gif':
+    //      sendGifMessage(senderID);
+    //      break;
+    //
+    //    case 'audio':
+    //      sendAudioMessage(senderID);
+    //      break;
+    //
+    //    case 'video':
+    //      sendVideoMessage(senderID);
+    //      break;
+    //
+    //    case 'file':
+    //      sendFileMessage(senderID);
+    //      break;
+    //
+    //    case 'button':
+    //      sendButtonMessage(senderID);
+    //      break;
+    //
+    //    case 'generic':
+    //      sendGenericMessage(senderID);
+    //      break;
+    //
+    //    case 'receipt':
+    //      sendReceiptMessage(senderID);
+    //      break;
+    //
+    //    case 'quick reply':
+    //      sendQuickReply(senderID);
+    //      break;
+    //
+    //    case 'read receipt':
+    //      sendReadReceipt(senderID);
+    //      break;
+    //
+    //    case 'typing on':
+    //      sendTypingOn(senderID);
+    //      break;
+    //
+    //    case 'typing off':
+    //      sendTypingOff(senderID);
+    //      break;
+    //
+    //    case 'account linking':
+    //      sendAccountLinking(senderID);
+    //      break;
+    //
+    //    default:
+    //      sendTextMessage(senderID, messageText);
+    //  }
+    //} else if (messageAttachments) {
+    //  sendTextMessage(senderID, "Message with attachment received");
+    //}
   }
 }
-
 
 /*
  * Delivery Confirmation Event
