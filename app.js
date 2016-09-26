fa4a232c4af519c71c16658ed3930fb8f95e9937
ZorinAlex <zorinuk@gmail.com>
2016-09-26 21:36:24 +0300

    test

--- app.js
+++ app.js
@@ -250,12 +250,10 @@ function receivedMessage(event) {
   //}
 
   if (messageText) {
-    switch (messageText) {
-      case 'Hi':
-        sendQuickReply(senderID);
-        break;
-      case 'Hello':
-        sendTextMessage(senderID, metadata);
+    switch (messageText.toLowerCase()) {
+      case 'hi':
+      case 'hello':
+        sendTextMessage(senderID, 'Hi, I am weather bot!');
         break;
     }
     //  switch (messageText) {
@@ -693,36 +691,36 @@ function sendReceiptMessage(recipientId) {
  * Send a message with Quick Reply buttons.
  *
  */
-function sendQuickReply(recipientId) {
-  var messageData = {
-    recipient: {
-      id: recipientId
-    },
-    message: {
-      text: "What's your favorite movie genre?",
-      metadata: "DEVELOPER_DEFINED_METADATA",
-      quick_replies: [
-        {
-          "content_type":"text",
-          "title":"Action",
-          "payload":"DEVELOPER_DEFINED_PAYLOAD_FOR_PICKING_ACTION"
-        },
-        {
-          "content_type":"text",
-          "title":"Comedy",
-          "payload":"DEVELOPER_DEFINED_PAYLOAD_FOR_PICKING_COMEDY"
-        },
-        {
-          "content_type":"text",
-          "title":"Drama",
-          "payload":"DEVELOPER_DEFINED_PAYLOAD_FOR_PICKING_DRAMA"
-        }
-      ]
-    }
-  };
-
-  callSendAPI(messageData);
-}
+//function sendQuickReply(recipientId) {
+//  var messageData = {
+//    recipient: {
+//      id: recipientId
+//    },
+//    message: {
+//      text: "What's your favorite movie genre?",
+//      metadata: "DEVELOPER_DEFINED_METADATA",
+//      quick_replies: [
+//        {
+//          "content_type":"text",
+//          "title":"Action",
+//          "payload":"DEVELOPER_DEFINED_PAYLOAD_FOR_PICKING_ACTION"
+//        },
+//        {
+//          "content_type":"text",
+//          "title":"Comedy",
+//          "payload":"DEVELOPER_DEFINED_PAYLOAD_FOR_PICKING_COMEDY"
+//        },
+//        {
+//          "content_type":"text",
+//          "title":"Drama",
+//          "payload":"DEVELOPER_DEFINED_PAYLOAD_FOR_PICKING_DRAMA"
+//        }
+//      ]
+//    }
+//  };
+//
+//  callSendAPI(messageData);
+//}
 
 /*
  * Send a read receipt to indicate the message has been read
